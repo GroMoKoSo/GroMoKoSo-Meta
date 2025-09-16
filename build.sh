9e0a0e27e2b538360b72cd4c149b25e8e49f742a
Josia Menger <josia.menger@gmx.de>
2025-09-16 14:54:38 +0200

    build: add local build files

---
+++ build.sh
@@ -0,0 +1,14 @@
+# UI
+git clone git@git.thm.de:softwarearchitektur-wz-ss24/studentswa2025/enton/userinterface.git || cd userinterface; git pull; cd ..
+
+# UserManagement
+git clone git@git.thm.de:softwarearchitektur-wz-ss24/studentswa2025/enton/usermanagement.git || cd usermanagement; git pull; cd ..
+
+# ApiManagment
+git clone git@git.thm.de:softwarearchitektur-wz-ss24/studentswa2025/enton/apimanagement.git || cd apimanagement; git pull; cd ..
+
+# McpManagement
+git clone git@git.thm.de:softwarearchitektur-wz-ss24/studentswa2025/enton/mcpmanagement.git || cd mcpmanagement; git pull; cd ..
+
+# Spec2Tool
+git clone git@git.thm.de:softwarearchitektur-wz-ss24/studentswa2025/enton/spec2tool.git || cd spec2tool; git pull; cd ..
